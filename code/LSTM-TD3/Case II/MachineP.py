79ee82a4cd3a043e8b6c7857691715e4d21dac2d
peterirenn <62362171+peterirenn@users.noreply.github.com>
2022-04-12 11:42:30 -0700

    Add files via upload

---
+++ code/LSTM-TD3/Case II/MachineP.py
@@ -0,0 +1,285 @@
+"""
+Machine class.
+Serves as interface between agent functions and experimental implementation.
+
+Modified from server class framework from:
+    "Reinforcement Learning for Active Flow Control in Experiments" (https://arxiv.org/abs/2003.03419)
+    https://github.com/LiuYangMage/RLFluidControl/blob/master/server/server.py
+    By Dixia Fan, Liu Yang, Michael S Triantafyllou, and George Em Karniadakis
+
+Updated to remove XML server aspects, for TF2, and for LSTM-TD3.
+
+Modified by Peter Renn.
+"""
+import os
+import pickle
+import tensorflow as tf
+import numpy as np
+from agent_TD3_keras import TD3
+from utils import KalmanFilter, NoneFilter
+from datetime import datetime
+import argparse
+
+class MachineP():
+    """
+        'Machine' to interface between the agent functions and application.
+    """
+    def __init__(self, filter, *args, **kwargs):
+        self.action_dim = 1
+        # Dimension of state space
+        self.state_dim = 9
+        # Dimensionn of load
+        self.load_dim = 1
+        # Create agent_TD3 with the given state and action dimensions, and exploreation noise?
+        self.agent = TD3(self.state_dim, self.action_dim)#, explore_noise = explore_noise)
+        # Stores unobservable state (i.e. the load)
+        self.load_record = []
+        # Stores unfiltered loads
+        self.unfiltered_load_record = []
+        # Stores recorded states (filtered)?
+        self.state_record = [] # filtered.
+        # Stores recorded states (not filtered)?
+        self.unfiltered_state_record = []
+        # Stores actions taken
+        self.action_record = []
+        # stores timesteps
+        self.time_record = []
+        # Means for subtracting
+        self.state_mean = np.zeros((1,self.state_dim))
+        self.load_mean = np.zeros((1, self.load_dim))
+        # Sets the filter to be used
+        self.filter_name = filter
+        if self.filter_name == "Kalman":
+            self.filter = KalmanFilter()
+            self.load_filter = KalmanFilter()
+            self._stamp("Using Kalman Filter")
+        elif self.filter_name == "None":
+            self.filter = NoneFilter()
+            self.load_filter = NoneFilter()
+            self._stamp("Using No Filter")
+        else:
+            raise NotImplementedError
+        # Sets the directory for saving
+        self.save_model_dir = "save"
+        self.save_data_dir = "save_data"
+        self.save_eval_dir = "save_eval"
+
+    def _stamp(self, string):
+        """
+        prints a time-stamped statement.
+
+        args:
+        	string: string to print a time-stamped statement
+        """
+        time = "UTC " + datetime.utcnow().isoformat(sep=" ", timespec="milliseconds") + " "
+        print(time + string, flush = True)
+
+    def _reward_func(self, this_state, this_load, next_state, next_load, last_load):
+        """
+        reward function used.
+
+        args:
+        	this_state: state for current timestep
+            this_load: load for current timestep
+            next_state: state for subsequent timestep
+            next_load: load for subsequent timestep
+            last_load: load for previous timestep
+        """
+        reward_raw = -10*(next_load)**2
+        return reward_raw
+
+    def _init(self, episode_count):
+        """
+        initialize agent
+
+        args:
+            episode_count: episode number from which to try to restore.
+        """
+        try:
+            self._restore(episode_count)
+            return True
+
+        except Exception:
+            self.agent.reset_agent()
+            self._stamp("Initialized!")
+            return False
+
+    def _start_episode(self,state_mean, load_mean):
+        """
+        starts episode and resets buffers, records, and filters
+
+        args:
+            state_mean: mean state calculated/measured at beginning
+            load_mean: mean load calculated/measured at beginning
+        """
+        self.state_mean = state_mean #(1, state_dim)
+        self.load_mean = load_mean
+        self.load_record = []
+        self.unfiltered_load_record = []
+        self.state_record = []
+        self.unfiltered_state_record = []
+        self.action_record = []
+        self.time_record = []
+        self.agent.reset_episode()
+        self.filter.reset()
+        self.load_filter.reset()
+        self._stamp("Episode Start!")
+        return True
+
+
+    def _start_eval(self,state_mean, load_mean):
+        """
+        starts evaluation period and resets buffers, records, and filters
+
+        args:
+            state_mean: mean state calculated/measured at beginning
+            load_mean: mean load calculated/measured at beginning
+        """
+        self.state_mean = state_mean
+        self.load_mean = load_mean
+        self.load_record = []
+        self.unfiltered_load_record = []
+        self.state_record = []
+        self.unfiltered_state_record = []
+        self.action_record = []
+        self.time_record = []
+        self.filter.reset()
+        self.load_filter.reset()
+        self._stamp("Eval Start!")
+        return True
+
+    def _request_action(self, raw_data, raw_load, obs_buf, act_buf, stochastic):
+        #
+        calibrated_load = raw_load - self.load_mean
+        # Set the calibrated state to the raw value minus the mean
+        calibrated_state = raw_data - self.state_mean
+        # Filter the calibrated state
+        filtered_state = self.filter.estimate(calibrated_state)
+        # Filter the calibrated load
+        filtered_load = self.load_filter.estimate(calibrated_load)
+        # Get action
+        action = self.agent.get_action(filtered_state, obs_buf, act_buf, stochastic = stochastic)
+        self.unfiltered_load_record.append(calibrated_load)
+        self.load_record.append(np.float64(filtered_load))
+        self.unfiltered_state_record.append(calibrated_state)
+        self.state_record.append(filtered_state)
+        self.action_record.append(np.float64(action))
+        raw_action = action[0,:].numpy().tolist()
+        return raw_action
+
+    def _request_stochastic_action(self, raw_data, raw_load, obs_buf, act_buf):
+    """
+    requests stochastic action for episodes.
+
+    args:
+        raw_data: raw (unfiltered) state data
+        raw_load: raw (unfiltered) load data_
+        obs_buf: observation buffer
+        act_buf: action buffer
+    """
+        return self._request_action(raw_data, raw_load, obs_buf, act_buf, True)
+
+    def _request_deterministic_action(self, raw_data, raw_load, obs_buf, act_buf):
+    """
+    requests deterministic action for eval.
+
+    args:
+        raw_data: raw (unfiltered) state data
+        raw_load: raw (unfiltered) load data_
+        obs_buf: observation buffer
+        act_buf: action buffer
+    """
+        return self._request_action(raw_data, raw_load, obs_buf, act_buf, False)
+
+    def _train(self, steps):
+    """
+    trains agent and saves episode.
+
+    args:
+        steps: number of steps to train for
+    """
+        self._stamp("Training..")
+        if not os.path.exists(self.save_data_dir):
+            os.mkdir(self.save_data_dir)
+        np.savez(self.save_data_dir + "/data_{}.npz".format(self.agent.episode_count),
+                    state = np.array(self.state_record),
+                    unfiltered_state = np.array(self.unfiltered_state_record),
+                    action = np.array(self.action_record),
+                    unfiltered_load  = np.array(self.unfiltered_load_record),
+                    load = np.array(self.load_record),
+                    time = np.array(self.time_record))
+        # Get the length of the state records
+        record_length = len(self.state_record)
+        # Record the state record length
+        self._stamp("Length of Record: " + str(record_length))
+        # For each action and state, calculate the reward and store it
+        for i in range(20, record_length-1):
+            reward = self._reward_func(self.state_record[i],
+                                       self.load_record[i],
+                                       self.state_record[i+1],
+                                       self.load_record[i+1],
+                                       self.load_record[i-1])
+            self.agent.replay_buffer.store(self.state_record[i],
+                                        self.action_record[i],
+                                        reward,
+                                        self.state_record[i+1],
+                                        0)
+
+        # Stamp that training has started
+        self._stamp("Training Start!")
+        # Train for each step we saw
+        for i in range(steps):
+            self.agent.train_iter()
+        self._stamp("Training End!")
+        return True
+
+
+    def _save(self, dummy = None):
+    """
+    saves agent.
+    """
+        if not os.path.exists(self.save_model_dir):
+            os.mkdir(self.save_model_dir)
+        self.agent.save_model()
+
+        pickle_out = open(self.save_model_dir + "/{}.pickle".format(self.agent.episode_count),"wb")
+        pickle.dump(self.agent.replay_buffer, pickle_out)
+        pickle_out.close()
+        self._stamp("Saved Episode {}!".format(self.agent.episode_count))
+        return True
+
+
+    def _restore(self,episode_count):
+    """
+    restores agent from previous run.
+
+    args:
+        episode_count: episode number
+    """
+        self.agent.saver.restore(self.agent.sess, self.save_model_dir + "/{}.ckpt".format(episode_count))
+        pickle_in = open(self.save_model_dir + "/{}.pickle".format(episode_count),"rb")
+        self.agent.replay_buffer = pickle.load(pickle_in)
+        self.agent.episode_count = episode_count
+        self._stamp("Restored from Episode {}!".format(episode_count))
+        return True
+
+
+    def _save_eval(self):
+    """
+    saves evaluation
+    """
+        if not os.path.exists(self.save_eval_dir):
+            os.mkdir(self.save_eval_dir)
+        np.savez(self.save_eval_dir + "/data_{}.npz".format(self.agent.episode_count),
+                    state = np.array(self.state_record),
+                    unfiltered_state = np.array(self.unfiltered_state_record),
+                    action = np.array(self.action_record),
+                    unfiltered_load  = np.array(self.unfiltered_load_record),
+                    load = np.array(self.load_record),
+                    time = np.array(self.time_record))
+        return True
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="machine for wing")
+    parser.add_argument("-fil", "--filter", choices=["None", "Kalman"], default="Kalman")
+    args = parser.parse_args()
---
+++ code/LSTM-TD3/Case II/RunTests_Pressure.py
@@ -0,0 +1,341 @@
+"""
+P. Renn
+Running the Machine class
+Edited to not include the pressure values
+to check if they're confusing the algorithm
+"""
+import os
+import pickle
+import tensorflow as tf
+import numpy as np
+from agent_TD3_keras import TD3
+from MachineP import MachineP
+from utils import KalmanFilter, NoneFilter
+from datetime import datetime
+import argparse
+import serial
+import nidaqmx
+import time
+import sys
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="machine for wing")
+    parser.add_argument("-fil", "--filter", choices=["None", "Kalman"], default="None")
+    args = parser.parse_args()
+
+    # Initialize new machine object...
+    machine = MachineP(**args.__dict__)
+    machine._init(0)
+
+    ## Number of Pressure Sensors
+    n_sensors = 9
+    # Time to run each episode for (in second)
+    T = 1000
+    # Time spent in episode
+    t = 0
+    # Number of steps to train for
+    n_steps = 100
+    # Max length of history to consider for LSTM
+    max_hist_len = 10
+    # Number of Episodes to run _request_stochastic_action
+    n_episodes = 200
+
+    ################################################
+    #               SERIAL SETUP                   #
+    ################################################
+    # Create serial object
+    ser = serial.Serial()
+    # Set baudrate
+    ser.baudrate = 115200
+    # Set port
+    ser.port = 'COM9'
+    # Set timeout
+    ser.timeout = 2
+    # Open port
+    ser.open()
+
+    if ser.is_open != True:
+        raise Exception("Serial Port not opened. ")
+
+    ser.reset_input_buffer()
+
+    try:
+        # Read in and ignore everyting that you see to ensure buffer is clear
+        while ser.in_waiting > 0:
+            ser.reset_input_buffer()
+            print(ser.in_waiting)
+    except KeyboardInterrupt:
+        ser.close()
+        pass
+
+    # Flush input buffer
+    ser.reset_input_buffer()
+    # Byte to initialize sensors - returned if sensors initialize properly.
+    init_byte = b":"
+    # Initialize the sensors
+    ser.write(init_byte)
+    # Check if enough time has passed for all sensors to return init_byte
+    try:
+        while ser.in_waiting < n_sensors:
+            machine._stamp("Waiting to confirm sensors init...")
+            machine._stamp("Number of sensors initialized: " + str(ser.in_waiting))
+            time.sleep(3)
+    except KeyboardInterrupt:
+        ser.close()
+        pass
+    machine._stamp("Number of sensors initialized: " + str(ser.in_waiting))
+
+    # Make sure each sensor is initialized.
+    for i in range(n_sensors):
+        check_init = ser.read()
+        if check_init != init_byte:
+            print(check_init)
+            machine._stamp("Warning: Wrong number of sensors initialized...")
+    machine._stamp("Sensors Initialized!")
+    time.sleep(3)
+    # Flush input buffer
+    ser.reset_input_buffer()
+
+    # Byte to zero sensors - returned if sensors zero proprely.
+    zero_byte = b"+"
+    # Make sure still open
+    if ser.is_open != True:
+        raise Exception("Serial Port not opened. ")
+    # # Zero the Sensors
+    ser.write(zero_byte)
+    # Check if enough time has passed for all sensors to return init_byte
+    try:
+        while ser.in_waiting < n_sensors:
+            machine._stamp("Waiting to confirm all sensors zeroed...")
+            machine._stamp("Number of sensors zeroed: " + str(ser.in_waiting))
+            # Zero the sensors
+            time.sleep(10)
+    except KeyboardInterrupt:
+        ser.close()
+        pass
+    # Make sure that each sensor is zeroed
+    for i in range(n_sensors):
+        check_zero = ser.read()
+        if check_zero != zero_byte:
+            print(check_zero)
+            ser.close()
+    machine._stamp("All sensors zeroed!")
+
+    ser.write(b";410")
+    # Set speed
+    ser.write(b"<816")
+    sensor_mean = np.zeros((1,n_sensors))
+    # Should return one set of data (For some reason?) - use that as zero
+    for i in range(n_sensors):
+        machine.state_mean[0,i] = ser.readline()
+    time.sleep(2)
+
+    # Flush input buffer of confirmation messages
+    ser.reset_input_buffer()
+
+    # Set up DAQ. Find the zero point.
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        task.ai_channels.ai_range_high = 10
+        task.ai_channels.ai_range_low = -10
+        task.start()
+        loads_to_mean = np.zeros(10)
+        for l in range(10):
+            loads_to_mean[l] = np.mean(task.read(1000))
+        machine.load_mean[0,0] = np.mean(loads_to_mean)
+
+        print(machine.load_mean[0,0])
+        input("Press Enter to start...")
+        dont_train = False
+        try:
+            while True:
+                #####
+                # Train episode
+                #####
+
+                # There is no error, yet...
+                is_err = False
+                # Tell pressure sensor to come back online
+                ser.dtr = True
+                # Flush input Serial
+                ser.reset_input_buffer()
+                # Start episode with known pressure voltage offset for mean state
+                machine._start_episode(machine.state_mean, machine.load_mean)
+                # Start reading data
+                ser.write(b";410")
+                # Make numpy array to hold state info
+                raw_data = np.zeros((1, machine.state_dim))
+                # Make numpy array to hold load info
+                raw_load = np.zeros((1, 1))
+                # Make buffers
+                obs_buf = np.zeros((1, max_hist_len, machine.state_dim))
+                act_buf = np.zeros((1, max_hist_len, machine.action_dim))
+                # Track length of buffers
+                bf_len = 0
+                # Start counting...
+                start = time.time()
+                t_elapsed = 0
+                counter = 0
+
+                while counter < T:
+                    counter = counter + 1
+                    timer = time.time()
+                    machine.time_record.append(t_elapsed)
+                    # First element is the voltage from the loadcell
+                    raw_load[0,0] = task.read()
+
+                    try:
+                        for i in range(n_sensors):
+                            raw_data[0, i] = ser.readline()
+                    except:
+                        machine._stamp("Sensor #" + str(i) + "Fault")
+                        machine._stamp("Unexpected Error: " + str(sys.exc_info()[0]))
+                        # Check if there is an error
+                        is_err = True
+                        ser.dtr = False
+                    # Request an action - action will be a number from -1 to 1
+                    a = machine._request_stochastic_action(raw_data, raw_load, obs_buf, act_buf)
+
+                    if bf_len < max_hist_len:
+                        obs_buf[0, bf_len, :] = machine.state_record[-1]
+                        act_buf[0, bf_len, :] = machine.action_record[-1]
+                        bf_len = bf_len + 1
+                    else:
+                        obs_buf[0, :max_hist_len - 1, :] = obs_buf[0, 1:, :]
+                        act_buf[0, :max_hist_len - 1, :] = act_buf[0, 1:, :]
+                        obs_buf[0, max_hist_len - 1, :] = machine.state_record[-1]
+                        act_buf[0, max_hist_len - 1, :] = machine.action_record[-1]
+                    # Convert actions to a position ranging from -410 to 410 (correspons to [-40^o, +40^o])
+                    servo_pos = int(np.rint(a[0] * 410) + 410)
+
+                    # If it's more than 10 just attach it and send it.
+                    if servo_pos >= 100:
+                        servo_cmd = ";" + str(servo_pos)
+                    elif servo_pos >= 10:
+                        servo_cmd = ";0" + str(servo_pos)
+                    else:
+                        servo_cmd = ";00" + str(servo_pos)
+                    ser.write(bytearray(servo_cmd,'utf-8'));
+                    # Time stuff
+                    t_elapsed = time.time() - timer
+                    if (0.012 - t_elapsed > 0):
+                        time.sleep(0.012 - t_elapsed)
+
+                # Reset the position
+                ser.write(b";410")
+                # Tell pressure sensor to stop
+                ser.dtr = False
+                if machine.agent.episode_count == 201:
+                    dont_train = True
+                    T = 4000
+                # If there is an error, skip training
+                if is_err:
+                    # Say what's happening..
+                    machine._stamp('Restarting Episode ' + str(machine.agent.episode_count) + '!')
+                    # So that the next "_start_episode" has the correct count
+                    machine.agent.episode_count -= 1
+
+                    # If there is an error, don't train or save, just restart
+                    continue
+
+                # Train
+                if dont_train == False:
+                    machine._train(n_steps)
+                    # Save what you've done
+                    machine._save()
+                else:
+                    machine._save()
+
+                # There is no error, yet...
+                is_err = False
+                # Tell pressure sensor to come back online
+                ser.dtr = True
+                # Flush input Serial
+                ser.reset_input_buffer()
+                # Start episode with known pressure voltage offset for mean state
+                machine._start_eval(machine.state_mean, machine.load_mean)
+                # Start reading data
+                ser.write(b";410")
+                # Make numpy array to hold state info
+                raw_data = np.zeros((1, machine.state_dim))
+                # Make numpy array to hold load info
+                raw_load = np.zeros((1, 1))
+                # Make buffers
+                obs_buf = np.zeros((1, max_hist_len, machine.state_dim))
+                act_buf = np.zeros((1, max_hist_len, machine.action_dim))
+                # Track length of buffers
+                bf_len = 0
+                # Start counting...
+                start = time.time()
+                t_elapsed = 0
+                counter = 0
+                # Make array to hold pressure before averaging
+                #p_store = np.zeros((1,n_sensors))
+                while counter < T:
+                    counter = counter + 1
+                    timer = time.time()
+                    machine.time_record.append(t_elapsed)
+                    # First element is the voltage from the loadcell
+                    raw_load[0,0] = task.read()
+
+                    # Rest is pressure values
+                    try:
+                        for i in range(n_sensors):
+                            raw_data[0, i] = ser.readline()
+                    except:
+                        machine._stamp("Sensor #" + str(i) + "Fault")
+                        machine._stamp("Unexpected Error: " + str(sys.exc_info()[0]))
+                        # Check if there is an error
+                        is_err = True
+                        ser.dtr = False
+
+                    # Request an action - I expect that the action will be a number from -1 to 1
+                    a = machine._request_deterministic_action(raw_data, raw_load, obs_buf, act_buf)
+
+                    if bf_len < max_hist_len:
+                        obs_buf[0, bf_len, :] = machine.state_record[-1]
+                        act_buf[0, bf_len, :] = machine.action_record[-1]
+                        bf_len = bf_len + 1
+                    else:
+                        obs_buf[0, :max_hist_len - 1, :] = obs_buf[0, 1:, :]
+                        act_buf[0, :max_hist_len - 1, :] = act_buf[0, 1:, :]
+                        obs_buf[0, max_hist_len - 1, :] = machine.state_record[-1]
+                        act_buf[0, max_hist_len - 1, :] = machine.action_record[-1]
+
+                    # Convert actions to a position ranging from -410 to 410
+                    servo_pos = int(np.rint(a[0] * 410) + 410)
+
+                    # If it's more than 10 just attach it and send it.
+                    if servo_pos >= 100:
+                        servo_cmd = ";" + str(servo_pos)
+                    elif servo_pos >= 10:
+                        servo_cmd = ";0" + str(servo_pos)
+                    else:
+                        servo_cmd = ";00" + str(servo_pos)
+                    ser.write(bytearray(servo_cmd,'utf-8'));
+                    # Time stuff
+                    t_elapsed = time.time() - timer
+                    if (0.012 - t_elapsed > 0):
+                        time.sleep(0.012 - t_elapsed)
+                # Reset the position
+                ser.write(b";410")
+                # Tell pressure sensor to stop
+                ser.dtr = False
+                machine._save_eval()
+                machine._stamp('Saved eval ' + str(machine.agent.episode_count) + '!')
+                # If there is an error, skip training
+                if is_err:
+                    # Say what's happening..
+                    machine._stamp('Restarting Episode ' + str(machine.agent.episode_count) + '!')
+                    # So that the next "_start_episode" has the correct count
+                    machine.agent.episode_count -= 1
+
+                    # If there is an error, don't train or save, just restart
+                    continue
+
+        except KeyboardInterrupt:
+            ser.close()
+            pass
+        task.stop()
+    """ """
+    ser.close()
